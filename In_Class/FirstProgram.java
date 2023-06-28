bfe8956ba16ba64a9aa1805445ff4e77a268c3c9
Ali Akbari <ali.akbari1@ucalgary.ca>
2023-06-28 11:22:16 -0600

    Tutorial 1

---
+++ In_Class/FirstProgram.java
@@ -0,0 +1,15 @@
+package In_Class;
+
+
+public class FirstProgram {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello Reader");
+        int answer;
+        answer = 2*3;
+        System.out.println("Answer is " + answer);
+
+    }
+    
+}
