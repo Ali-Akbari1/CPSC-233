737b9ab125c5ce18c1a509657e3aeb1b3af06f70
Ali Akbari <ali.akbari1@ucalgary.ca>
2023-06-28 11:21:01 -0600

    Tutorial 1

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+
+
+public class FirstProgram {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello Reader");
+        int answer;
+        answer = 2*3;
+        System.out.println("Answer is " + answer);
+
+    }
+    
+}
