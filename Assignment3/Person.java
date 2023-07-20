e975ac2169a8c120e8718866d058cf52e6fdc171
Ali <alimustanserakbari@gmail.com>
2023-07-19 18:23:02 -0600

    Working on the Assignment 3 July 19

--- Assignment3/Person.java
+++ Assignment3/Person.java
@@ -1,7 +1,56 @@
 package Assignment3;
 
 public class Person {
-    int limbs;
+
+    //Instance variables
+    private double gpa;
+    private int age;
+    private String name;
+
+    // Constructor
+    public Person(double initialGPA, int initialAge, String InitialName){
+        setGPA(initialGPA);
+        setAge(initialAge);
+        setName(InitialName);
+
+    }
+
+
+    // Set Methods
+    public void setGPA(double newGPA){
+        gpa = newGPA;
+
+    }
+
+    public void setAge(int newAge){
+        age = newAge;
+    }
+
+    public void setName(String newName){
+        name = newName;
+
+    }
+
+    // Get Methods
+    public double getGPA(){
+        return gpa;
+
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getStudentGPA(){
+        gpa = getGPA();
+         
+    }
+
+
     public class Boy extends Person{
 
     }
