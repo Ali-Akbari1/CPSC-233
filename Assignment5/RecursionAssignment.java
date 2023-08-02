d27a57abaa83edc0435f2b5b97d7af1369a6f7ae
Ali <alimustanserakbari@gmail.com>
2023-08-02 14:42:30 -0600

    Almost Done Assignment 5 August 2

--- Assignment5/RecursionAssignment.java
+++ Assignment5/RecursionAssignment.java
@@ -8,18 +8,64 @@ public class RecursionAssignment {
         if (n ==0){
             return 0;
         }
-
         
+        return n%10+(addDigits(n/10));
+    }
 
-        n = n % 10;
+    public static int sumEvenNumbers(int n){
 
-        System.out.println(n);
-        
-        return n+(addDigits(n/10));
+        if (n == 0){
+            return 0;
+        }
+        else if (n % 2 == 0){
+            return n +sumEvenNumbers(n-1);
+        }
+        else{
+            return sumEvenNumbers(n-1);
+        }
+
+
+    }
+
+    public static int countVowels(String s){
+
+        if (s.length() == 0) return 0;
+
+
+
+        int temp = countVowels(s.substring(1));
+
+        char c = s.charAt(0);
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || 
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            temp++;
+        }
+
+        return temp;
+    }
+
+
+    public static String removeVowels(String s){
+        if (s.length() == 0) return "";
+
+        String temp = removeVowels(s.substring(1));
+
+        char c = s.charAt(0);
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || 
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            return temp;
+        }
+
+        return c + temp;
     }
 
     public static void main(String[] args){
         System.out.println(addDigits(12345));
+        System.out.println(sumEvenNumbers(16));
+        System.out.println(countVowels("Mississippi"));
+        System.out.println(removeVowels("HelloYou"));
     }
     
 }
